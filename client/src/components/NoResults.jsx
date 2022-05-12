92a1aad96d3ec8617e5ed0b4938734a71f6c89d5
ArmaanSingh98 <armaandsingh@gmail.com>
2022-05-11 20:19:16 -0400

    Update NoResults.jsx

--- client/src/components/NoResults.jsx
+++ client/src/components/NoResults.jsx
@@ -4,8 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NoResults() {
     return (
-    <div className="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>No providers are currently streaming criteria.</strong> Be sure to try again later!
+    <div class = "text-center">
+        <div className="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>No providers are currently streaming criteria. Be sure to try again later!</strong>
+        </div>
     </div>
     )
 }
