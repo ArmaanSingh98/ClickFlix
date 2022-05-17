77e8e64c3ac45a28171deda2d96c7d1cd1b1034d
ArmaanSingh98 <armaandsingh@gmail.com>
2022-05-17 17:23:34 -0400

    Fixed an issue where the name of the tv show when displaying the tv shows werent showing
    Changed title to name in the props.listitem.title to props.listitem.name

--- client/src/components/TVCards.jsx
+++ client/src/components/TVCards.jsx
@@ -9,7 +9,7 @@ function TVCards (props) {
                 <div className="card cardColor " style={{width: '18rem'}}  onClick={() => props.pickTV(props.listItem.id)}>
                     <img className="card-img-top" src={'https://image.tmdb.org/t/p/original'+props.listItem.poster_path} alt="Card image cap"/>
                     <div className="card-body">
-                        <p className="card-text">{props.listItem.title}</p>
+                        <p className="card-text">{props.listItem.name}</p>
                     </div>
                 </div>
             )
@@ -18,7 +18,7 @@ function TVCards (props) {
                 <div className="card cardColor " style={{width: '18rem'}} onClick={() => props.pickTV(props.listItem.id)}>
                     <img className="card-img-top" src="https://media.istockphoto.com/vectors/movie-time-vector-illustration-cinema-poster-concept-on-red-round-vector-id911590226" alt="Card image cap"/>
                     <div className="card-body">
-                        <p className="card-text">{props.listItem.title}</p>
+                        <p className="card-text">{props.listItem.name}</p>
                     </div>
                 </div>
             )
